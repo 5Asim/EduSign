9a332dac61025f8cc7e7c84dd507b9ed2a91ff5c
Rohit-Karki <rohitkarki804@gmail.com>
2024-09-16 15:25:15 +0545

    text-to-Pose

--- text_to_pose_scrapper.py
+++ text_to_pose_scrapper.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import requests
 
 # Initialize WebDriver
-driver = webdriver.Chrome(executable_path='path_to_chromedriver')
-driver.get('sign.mt')
+# driver = webdriver.Chrome(executable_path='path_to_chromedriver')
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser.get('https://sign.mt')
 
 # Wait for the page to load
 time.sleep(5)
 
 # Enter text in the input field
-input_field = driver.find_element(By.XPATH, '//*[@id="desktop"]')
+input_field = browser.find_element(By.XPATH, '//*[@id="desktop"]')
 text = "Hello, how are you? I am fine. Tell me about your day. How was it?"
 input_field.send_keys(text)
 
@@ -19,7 +22,7 @@ input_field.send_keys(text)
 time.sleep(1000)
 
 # Find the video element and download it
-video = driver.find_element(By.XPATH, '//*[@id="content"]/app-spoken-to-signed/app-signed-language-output/video')
+video = browser.find_element(By.XPATH, '//*[@id="content"]/app-spoken-to-signed/app-signed-language-output/video')
 video_url = video.get_attribute('src')
 
 # Download the video
@@ -28,4 +31,4 @@ with open('downloaded_video.mp4', 'wb') as f:
     f.write(video_response.content)
 
 # Close the browser
-driver.quit()
+browser.quit()
