291b3caf2ec3a1fe45b95fb03ec4afaaa2dd031d
5Asim <ashim.shrestha.55@gmail.com>
2024-09-20 20:29:14 +0545

    some changes- Asim

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+from text_audio import generate_audio_sync
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
+ 
+
+
+# Define voice options
+VOICES = ['en-US-GuyNeural', 'en-US-JennyNeural']
+
 
 # Endpoint to receive the transcript
 @app.route('/api/transcript', methods=['POST'])
@@ -27,6 +36,30 @@ def receive_transcript():
             return jsonify({"error": "No transcript provided"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+# Convert to Audio:
+@app.route('/api/transcript/audio', methods=['POST'])
+def transcriptToaudio():
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+
+        # Extract the transcript from the data
+        transcript = data.get('transcript')
+
+        if transcript:
+            # Call the function to generate audio
+            generate_audio_sync(transcript, VOICES[1])
+
+            # Send the audio file back to the client
+            return send_file("test.mp3", mimetype='audio/mp3')
+        else:
+            return jsonify({"error": "No transcript provided"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
 
 # Run the server
 if __name__ == '__main__':
